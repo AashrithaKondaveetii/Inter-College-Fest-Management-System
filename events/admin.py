8cf5e16449495da583f10974d7dec02e2bebd6e9
Aashritha Kondaveeti <146298266+AashrithaKondaveetii@users.noreply.github.com>
2023-12-08 18:46:15 -0800

    modified admin file

--- events/admin.py
+++ events/admin.py
@@ -1,2 +1,12 @@
 from django.contrib import admin
 
+from .models import Venue
+from .models import Members
+
+admin.site.register(Members)
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+	list_display = ('name', 'address', 'phone')
+	ordering =('name',)
+	search_fields = ('name', 'address')
