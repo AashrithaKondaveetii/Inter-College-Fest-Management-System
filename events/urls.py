8d14484fefd049d8197323496a044b35e133faa3
Aashritha Kondaveeti <146298266+AashrithaKondaveetii@users.noreply.github.com>
2023-12-08 17:56:43 -0800

    Updated Urls and Views files

--- events/urls.py
+++ events/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('<int:year>/<str:month>/', views.home, name="home"),
     path('add_event', views.add_event, name='add-event'),
+    path('events', views.all_events, name="list-events"),
+    path('add_venue', views.add_venue, name='add-venue'),
+    path('show_venue/<venue_id>', views.show_venue, name='show-venue'), 
 ]
--- events/views.py
+++ events/views.py
@@ -3,8 +3,13 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 from django.http import HttpResponseRedirect
-from .models import Event
-from .forms import EventForm, EventFormAdmin
+from .models import Event, Venue
+from .forms import VenueForm, EventForm, EventFormAdmin
+
+def all_events(request):
+	event_list = Event.objects.all().order_by('name')
+	return render(request, 'events/event_list.html',
+		{"event_list": event_list})
 
 def add_event(request):
 	submitted = False
@@ -33,6 +38,32 @@ def add_event(request):
 	return render(request, 'events/add_event.html',
 		{'form':form,'submitted':submitted})
 
+def add_venue(request):
+	submitted = False
+	if request.method == "POST":
+		form = VenueForm(request.POST)
+		if form.is_valid():
+			venue = form.save(commit=False)
+			venue.owner = request.user.id #logged in user
+			venue.save()
+			# form.save()
+			return HttpResponseRedirect('/add_venue?submitted=True')
+
+	else:
+		form = VenueForm
+		if 'submitted' in request.GET:
+			submitted = True
+	return render(request, 'events/add_venue.html',
+		{'form':form,'submitted':submitted})
+
+def show_venue(request, venue_id):
+	venue = Venue.objects.get(pk=venue_id)
+	venue_owner = User.objects.get(pk=venue.owner)
+	events = venue.event_set.all()
+	return render(request, 'events/show_venue.html', 
+		{'venue': venue,
+		'venue_owner':venue_owner,
+		'events':events})
 
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
 	name = "Honey"
