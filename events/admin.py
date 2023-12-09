b268177eecd389008ea663a8473eccc61fa9cce4
sainutheti <146263097+sainutheti@users.noreply.github.com>
2023-12-09 08:20:44 +0530

    updated admin file with event related code

--- events/admin.py
+++ events/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-
 from .models import Venue
 from .models import Members
+from .models import Event
 
 admin.site.register(Members)
+
 
 @admin.register(Venue)
 class VenueAdmin(admin.ModelAdmin):
 	list_display = ('name', 'address', 'phone')
 	ordering =('name',)
 	search_fields = ('name', 'address')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+	fields = (('name', 'venue'), 'event_date', 'description', 'manager', 'approved')
+	list_display = ('name', 'event_date', 'venue')
+	list_filter  = ('event_date', 'venue')
+	ordering =('-event_date',)
