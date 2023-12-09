b5f5034faeae14ee33b8eed02f2f5b31681915f0
sainutheti <146263097+sainutheti@users.noreply.github.com>
2023-12-09 07:04:58 +0530

    Modified urls.py and views.py and deleted unwanted files

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('<int:year>/<str:month>/', views.home, name="home"),
+    path('add_event', views.add_event, name='add-event'),
 ]
--- events/views.py
+++ events/views.py
@@ -2,7 +2,36 @@ from django.shortcuts import render
 import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
+from django.http import HttpResponseRedirect
+from .models import Event
+from .forms import EventForm, EventFormAdmin
 
+def add_event(request):
+	submitted = False
+	if request.method == "POST":
+		if request.user.is_superuser:
+			form = EventFormAdmin(request.POST)
+			if form.is_valid():
+				form.save()
+				return HttpResponseRedirect('/add_event?submitted=True')
+		else:
+			form = EventForm(request.POST)
+			if form.is_valid():
+				# form.save()
+				event = form.save(commit=False)
+				event.manager = request.user #logged in user
+				event.save()
+				return HttpResponseRedirect('/add_event?submitted=True')
+	else:
+		#Just going to the page, not submitting
+		if request.user.is_superuser:
+			form = EventFormAdmin
+		else:
+			form = EventForm
+		if 'submitted' in request.GET:
+			submitted = True
+	return render(request, 'events/add_event.html',
+		{'form':form,'submitted':submitted})
 
 
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
